42aea83154b49b4bd5d4b1ad954a68ea018899c5
Morning <morning@MStarss-iMac.local>
2022-11-29 12:33:37 +0800

    11

---
+++ Day11/JS/main.js
@@ -0,0 +1,155 @@
+// var baby = "Baby Shark, doo-doo, doo-doo";
+
+// console.log(baby);
+
+// console.log(baby);
+// console.log(baby);
+// console.log(baby);
+
+
+
+// var a = "Baby Shark, ";
+// var b = "doo-doo, ";
+// var c = "doo-doo";
+// var d = "Mommy Shark, ";
+
+// console.log(a + b + c);
+// console.log(a + b + c);
+// console.log(a + b + c);
+
+
+// let myNumber = 5;
+// let undify;
+// let nullVariable = null;
+// let myDecision = true;
+
+// // typeof
+// console.log(typeof 1);
+// console.log(typeof "Hello");
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+// console.log(myDecision);
+// console.log(typeof myDecision);
+
+/**
+ * alert gargana
+ * 
+ * x`2 + y`2 - z`3 / 240 * 175
+ */
+// const PI = 3.14;
+// let r = 45;
+// let circleLength = 2 * PI * r;
+// console.log(circleLength);
+
+// let height = 5;
+// let width = 6;
+// let h = 12;
+// let trapezoidArea = (height + width )/ 2 * h;
+// alert(trapezoidArea);
+
+// let yearOfBirth = prompt('please fill the Year');
+// let currentYear = 2022;
+// let old = (currentYear - yearOfBirth);
+// console.log(old);
+
+// if (0<=old && old <= 1) {
+//     console.log('Infact')
+// }
+// else if (1 < old && old <= 3){
+//     console.log ('Toddler')
+// }
+// else if (3 < old && old <= 5){
+//     console.log ('Preschool')
+// }
+// else if (5 < old && old <= 12){
+//     console.log ('GradeSchool')
+// }
+// else if (12 < old && old <= 18){
+//     console.log ('Teen')
+// }
+// else if (18 < old && old <= 21){
+//     console.log ('young Adult')
+// }
+// else {
+//     console.log ('Adult')
+// }
+
+
+            // easy 8 
+// let myNumber = prompt('please fill the Number');
+// console.log(myNumber)
+// if (myNumber%2  == 0){
+//     console.log('even')
+// }
+// else{
+//     console.log('odds');
+// }
+
+
+// ease 3
+// let myNumber = prompt('please fill the Number');
+// console.log(myNumber)
+// if (myNumber%3 == 0 || myNumber%7 == 0 ){   
+//     console.log('true')
+// }
+
+// else{
+//     console.log('false');
+// }
+
+// easy 5
+
+// let weight = prompt('fill the weight');
+// let height = prompt('fill the height meters');
+// let BMI = ( weight / (height * height) );
+// console.log(BMI);
+
+// if (BMI <18.5) {
+//     console.log('Under weight')
+// }
+// else if (18.5 < BMI && BMI <= 24.9){
+//     console.log ('normal')
+// }
+// else if (25 < BMI && BMI <= 29.9){ 
+//     console.log ('over height')
+// }
+// else if (30 < BMI && BMI <= 34.9){    
+//     console.log ('obesity')
+// }
+// else{    
+//     console.log ('Extreme obesity')
+// }
+
+let day = prompt('Fill the day');
+
+let dayNumber = Number(day);
+
+let dayName;
+console.log(typeof(dayNumber));
+
+ switch(dayNumber){
+    case 1:
+        dayName = 'Monday'
+        break;
+    case 2:
+        dayName = 'tue'
+        break;
+    case 3:
+        dayName = 'Wed'
+        break;
+    case 4:
+        dayName = 'Thurs'
+        break;  
+    case 5:
+        dayName = 'Fri'
+        break;
+    case 6:
+        dayName = 'Sat'
+        break;
+    case 7:
+        dayName = 'Sun'
+        break;
+    default:
+        dayName = 'Invalid day'
+ }
+ console.log(dayName);
