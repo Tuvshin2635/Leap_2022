7850304f03c561e0f09799ea01d5f32d3e73bb09
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-11-30 14:01:05 +0800

    assiagment1

--- JS Condition/Assignment-1/main.js
+++ JS Condition/Assignment-1/main.js
@@ -1,4 +1,5 @@
 
+// Assignment -1 - #1
 
  let i = 4 > 3; //true
  let iii = 4 < 3; //false
@@ -12,6 +13,9 @@
  let x = 4 == "4"; //true
  let xi = 4 === "4"; //false
  
+
+// Assignment -1 - #2
+
  console.log(i, iii, ii , iv, v, vi, vii, iix, ix, x, xi);
  
  
@@ -26,6 +30,8 @@
  let ten = !(4 === "4"); //true
  console.log(one, two, three, four, five, six, eight, nine, ten);
  
+ //Assignment -1 - #3
+
  let myAge = prompt('fill the your Age')
  let yourAge = prompt('fill the his/her Age')
  let old =(myAge - yourAge)
@@ -33,10 +39,30 @@
      if(myAge > yourAge){
          console.log('chi xugshin')
      }
-     if(myAge < yourAge){
+     else if(myAge < yourAge){
          console.log('chi zaluu')
      }
-     else (myAge = yourAge)
+     else if (myAge = yourAge) {
          console.log('nas chitsuu')
+    } else {}
      
- 
+//  Assignment -1 - #4
+
+
+let day=prompt('Fill the Day');
+
+if ( 1 <= day && day <= 5) {
+    alert('Ajliin udur')
+} else  if( 6 <= day && day <= 7 ) {
+    alert('Amraltiin udur')
+} else {
+    alert('Fill the number 1-7')
+}
+
+ //  Assignment -1 - #5
+
+ let salary = prompt('Fill the how many hours ?');
+ let hours = prompt('how much dollar for a hour?');
+ let wage = (salary * hours);
+ alert(wage);
+
