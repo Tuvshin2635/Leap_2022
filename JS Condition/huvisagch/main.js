781759f3edb617e2b8a6804378445046f731dd0f
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-01 08:04:45 +0800

    huvisagch

---
+++ JS Condition/huvisagch/main.js
@@ -0,0 +1,27 @@
+// #1 
+let myNumbers = ( a=85, b=75, c=96, d=69);
+let x = 0;
+if (a > 80) {
+    x = x + a;
+} if ( b >80) {
+    x = x + b;
+} if ( c >80) {
+    x = x + c;
+} if ( d >80) {
+    x = x + d;
+} 
+console.log('x =' , x);
+
+//#2
+let = ( e = 3, f=7, g=2, h=4);
+let y = 1;
+if ( e<5) {
+    y = y*e;
+} if ( f<5 ){
+    y = y * f
+}  if ( g<5 ){
+    y = y * g
+}  if ( h<5 ){
+    y = y * h
+}  
+console.log('y =' , y);
