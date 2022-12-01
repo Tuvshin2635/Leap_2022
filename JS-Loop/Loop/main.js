5b1650e267889214849941bde06df074abf50a17
Morning <morning@MStarss-iMac.local>
2022-12-01 12:37:43 +0800

    day13

---
+++ JS-Loop/Loop/main.js
@@ -0,0 +1,28 @@
+// #1
+console.log('loop starts');
+let count1 = 1;
+while (count1 <= 10) {
+    console.log(count1);
+    count1 = count1 + 1;
+}
+console.log('Loop ended');
+
+
+
+
+
+
+
+// let max = Number(prompt('Give me random number?'));
+// let count = 0;
+
+// let sum = 0;
+// while (count < max) {
+//     console.log(count);
+//     count = count + 1;
+//     sum = count + sum
+// }
+// console.log(sum);
+
+// max = 10, count 1 , sum=0
+iteration 1 :count = 1
