618c24bbbcef14954d5410bcbe875faf226ab746
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-01 13:40:31 +0800

    loop

---
+++ JS-Loop/Tegsh_too/main.js
@@ -0,0 +1,13 @@
+// #1
+console.log('loop starts');
+let count = 0;
+while (count <= 100) {
+    console.log(count);
+    count = count + 2;
+}
+console.log('Loop ended');
+
+
+
+
+// while loop ашиглан 0-100 хүртэлх тооны бүх тэгш тоог олох.
