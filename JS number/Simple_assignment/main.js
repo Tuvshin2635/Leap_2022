164e9daa6eccd264a77e0cd9d933d0e43cf6a80c
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-03 23:09:23 +0800

    traingle

--- JS number/Simple_assignment/main.js
+++ JS number/Simple_assignment/main.js
@@ -1,3 +1,2 @@
 console.log(Math.ceil(Math.random() * 100) + 1);
 console.log(Math.ceil(Math.random() * 255) + 50);
-
---
+++ JS number/general_triangle/main.js
@@ -0,0 +1,4 @@
+let x = prompt(' gurvaljinii a tal?')
+let y = prompt(' gurvaljinii a tal?')
+console.log(Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2))));
+
