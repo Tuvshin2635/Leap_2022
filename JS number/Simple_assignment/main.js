67ae7f57971230c5c1812b575eaf83d0797ba510
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-03 22:55:51 +0800

    ned_usa

--- JS number/Simple_assignment/main.js
+++ JS number/Simple_assignment/main.js
@@ -1,21 +1,3 @@
+console.log(Math.ceil(Math.random() * 100) + 1);
+console.log(Math.ceil(Math.random() * 255) + 50);
 
-console.log(Math.ceil(Math.random() * 100) + 1 ); 
-
-console.log(Math.ceil(Math.random() * 255) + 50 ); 
-
-
-
-
-// console.log(Math.random());
-
-// let x = 
-// console.log(Math.floor(Math.random() * 16 + 1));
-// console.log(Math.floor(Math.random() * (max - min) + min))) < 100);
-
-// let count = 5;
-
-// console.log((Math.floor(Math.random() * (max - min) + min)) <= 5);
-// console.log(count);
-// count = count + 1;
-
-// console.log(Math.ceil(Math.random())); 
