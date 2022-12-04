e923f2fc8f409556dc95a8f6dcfa8a3ddb7b3993
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-04 16:30:34 +0800

    numberdigit

---
+++ JS-Loop/number_digits/main.js
@@ -0,0 +1,20 @@
+// let a = prompt(" give me big number");
+// let count = 0;
+
+
+// while(a!==0){
+//     count  = count + 1;
+//     a = Math.floor(a/10);
+// }
+// console.log(count);
+
+// console.log(Math.floor(12.3)); //12 n
+
+let a = prompt('give number?');
+let count=0;
+
+for (;a!== 0; ) {
+    count = (count + 1);
+    a = Math.floor(a / 10);
+}     console.log(count);
+
