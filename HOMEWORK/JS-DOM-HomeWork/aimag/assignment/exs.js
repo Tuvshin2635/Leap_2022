fd35982ed737a78dca6c9b6900150c42d20153a2
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2022-12-13 22:22:43 +0800

    hw aupdate

--- HOMEWORK/JS-DOM-HomeWork/aimag/assignment/exs.js
+++ HOMEWORK/JS-DOM-HomeWork/aimag/assignment/exs.js
@@ -1,50 +1,50 @@
 console.log('Exercises');
-console.log( '===== 1 ======= ');
+console.log('===== 1 ======= ');
 console.log(document.getElementById("aimag-101").innerHTML);
 
-console.log( '===== 2 ======= ');
+console.log('===== 2 ======= ');
 
 console.log(document.getElementById('aimag-522').innerHTML);
 
-console.log( '===== 3 ======= ');
+console.log('===== 3 ======= ');
 
 console.log(document.getElementById('aimag-315').innerHTML);
 
-console.log( '===== 4 ======= ');
+console.log('===== 4 ======= ');
 document.getElementById("aimagNames").style = 'font-size: 30px; color: red; margin-top: 30px; padding: 20px';
-console.log( '===== 5 ======= ');
 
+console.log('===== 5 ======= ');
+console.log('Xangain bus id=1.. yellow');
+for (i = 0; i < document.getElementById('aimagNames').getElementsByTagName('li').length; i++) {
+    if (document.getElementById('aimagNames').getElementsByTagName('li')[i].id.includes('aimag-1')) {
+        console.log(document.getElementById('aimagNames').getElementsByTagName('li')[i].style = 'background-color: yellow');
+    }
+}
+console.log('Baruun bus id=2.. brown');
 
+for (let i = 0; i < document.getElementById('aimagNames').getElementsByTagName('li').length; i++) {
+    if (document.getElementById('aimagNames').getElementsByTagName('li')[i].id.includes('aimag-2')) {
+        console.log(document.getElementById('aimagNames').getElementsByTagName('li')[i].style = 'background-color: brown; color: blue;');
+    }
+}
 
+console.log('Zuun bus id=3.. gray');
 
+for (let i = 0; i < document.getElementById('aimagNames').getElementsByTagName('li').length; i++) {
+    if (document.getElementById('aimagNames').getElementsByTagName('li')[i].id.includes('aimag-3')) {
+        console.log(document.getElementById('aimagNames').getElementsByTagName('li')[i].style = 'background-color: gray; color: black;');
+    }
+}
 
+console.log('Tuviin bus id=4.. green');
 
+for (let i = 0; i < document.getElementById('aimagNames').getElementsByTagName('li').length; i++) {
+    if (document.getElementById('aimagNames').getElementsByTagName('li')[i].id.includes('aimag-4')) {
+        console.log(document.getElementById('aimagNames').getElementsByTagName('li')[i].style = 'background-color: green; color: white;');
+    }
+}
+console.log('Tuviin bus id=5.. pink');
 
+document.getElementById("aimag-522").style = 'border: 2px solid pink; color: blue; background-color: pink; border-radius:20px; font-size: 45px;'
+ 
 
-
-
-
-
-
-
-
-// document.getElementById("aimagNames").style = 'font-size: 40px;' .style = 'color: red';
-// document.getElementById("aimagNames").style = 'color: red'
-
-document.getElementById("aimag-101").style = 'border: 2px solid pink; color: blue; background-color: gray; border-radius:10px;'
-
-// for (let i = 0; i < document.getElementsByClassName("khangai").length; i++) {
-//     document.getElementsByClassName("khangai")[i].style = 'font-size: black; background-color: black; color:white;'
-// }
-
-// console.log(document.getElementById('aimagNames').id);
-
-// const Lilength = document.getElementById('aimagNames').getElementsByTagName('li').length;
-// console.log(document.getElementById('aimagNames').getElementsByTagName('li')[Lilength - 1].innerHTML);
-
-
-// for (i = 0; i < document.getElementById('aimagNames').getElementsByTagName('li').length; i++) {
-//     if (document.getElementById('aimagNames').getElementsByTagName('li')[i].id.includes('aimag-1')) {
-//         console.log(document.getElementById('aimagNames').getElementsByTagName('li')[i].style = 'background-color: yellow');
-//     }
-// }
--- HOMEWORK/JS-Object/Delguur/main.js
+++ HOMEWORK/JS-Object/Delguur/main.js
@@ -1,9 +1,31 @@
+// 1. Нийт борлуулалтын дүнг тооцоолох.
+// 2. Нийт борлуулагдсан барааны тоог тооцоолох.
+// 3. Хамгийн их зарагдсан 5 барааны жагсаалт 
+        // /боруулалтын үнийн дүнгээр/ харуулах.
+// 4. Хамгийн их зарагдсан 5 барааны жагсаалт 
+        // /боруулалтын тоо хэмжээгэр/ харуулах.
+// 5. Хамгийн бага зарагдсан 5 барааны жагсаалт 
+        // /боруулалтын үнийн дүнгээр/ харуулах.
+// 6. Хамгийн бага зарагдсан 5 барааны жагсаалт 
+//      /боруулалтын тоо хэмжээгээр/ харуулах.
+// 7. Хамгийн их зарагдсан /боруулалтын үнийн дүнгээр/ 
+        // 10 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд 
+            // эзлэх хувь харуулах.
+// 8. Хамгийн их зарагдсан /боруулалтын тоо хэмжээгээр/ 
+//      5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх хувь харуулах.
+// 9. Хамгийн бага зарагдсан /боруулалтын үнийн дүнгээр/ 
+        // 5 барааны боруулалтын үнийн дүнгийн нийт боруулалтанд 
+        // эзлэх хувь харуулах.
+// 10. Хамгийн бага зарагдсан /боруулалтын тоо хэмжээгээр/ 
+        // 5 барааны тоо хэмжээ нийт барааны тоо хэмжээнд эзлэх 
+        // хувь харуулах.
+
 const data = [
     {
         productName: "Bakery",
         unitPrice: 5000,
         amount: 200,
-        totalPrice: 500000,
+        totalPrice: 1000000,
         casherId: 1,
         date: '2022-11-01'
     },
@@ -11,7 +33,7 @@ const data = [
         productName: "Chocolate",
         unitPrice: 3000,
         amount: 18,
-        totalPrice: 54.000,
+        totalPrice: 54000,
         casherId: 1,
         date: '2022-11-01'
     },
@@ -19,7 +41,7 @@ const data = [
         productName: "candy",
         unitPrice: 1000,
         amount: 320,
-        totalPrice: 54000,
+        totalPrice: 320000,
         casherId: 1,
         date: '2022-11-01'
     },
@@ -39,4 +61,76 @@ const data = [
         casherId: 1,
         date: '2022-11-01'
     },
+    {
+        productName: "cook",
+        unitPrice: 3800,
+        amount: 94,
+        totalPrice: 357200,
+        casherId: 1,
+        date: '2022-11-01'
+    },
+    {
+        productName: "nogoo",
+        unitPrice: 6000,
+        amount: 50,
+        totalPrice: 300000,
+        casherId: 1,
+        date: '2022-11-01'
+    },
+    {
+        productName: "jims",
+        unitPrice: 8000,
+        amount:75,
+        totalPrice: 600000,
+        casherId: 1,
+        date: '2022-11-01'
+    },
+    {
+        productName: "davs",
+        unitPrice: 3800,
+        amount: 120,
+        totalPrice: 456000,
+        casherId: 1,
+        date: '2022-11-01'
+    },
 ];
+
+console.log( '===  SHOP ===' );
+
+let sum = 0;
+for ( let i=0; i<data.length; i++) {
+    sum = sum + data[i].totalPrice
+}     console.log('Нийт борлуулалт:  ' + sum);
+
+let sum2 = 0;
+for ( let i=0; i<data.length; i++) {
+    sum2 = sum2 + data[i].amount
+}     console.log('Нийт борлуулагдсан бараа:  ' + sum2);
+
+let sum3 = 0;
+let count = 5;
+for ( let i=0; i<data.length; i++) {
+    // Math.max(amount[i])
+    
+}   
+count = 0;
+for ( let i=0; i<data.length; i++) {
+    count= count + 1
+    console.log(count , Math.max(data[i].amount));
+}
+
+// var max = arr[0];
+// let maxValues = [];
+for (let i = 1; i < data.length; i++) {
+  if (data[i].amount > max) {
+    max = data[i].amount; 
+  }
+}
+console.log("Max is: " + max);
+console.log("total five max values is: " + maxValues);
+
+
+
+// console.log('Хамгийн их зарагдсан 5 бараа:  ' + Math.max(data[i].productName));
+
+
