1e84d5198bc5646055d5a4f0ebced988381dab0d
Morning <morning@MStarss-iMac.local>
2022-12-14 12:29:13 +0800

    todo

---
+++ HOMEWORK/Tuvshin_Todo_App/js/todo.js
@@ -0,0 +1,28 @@
+// Date part
+let presentTime = new Date();
+let shortDate = presentTime.getFullYear() + ' - ' + presentTime.getMonth() + ' - ' + presentTime.getDate();
+document.getElementById("presentTime").innerHTML = shortDate;
+
+// TODO APP
+let todoCreate = document.getElementById("todo-create");
+let todoList = document.getElementById("todo-list");
+let button = document.getElementById("add");
+
+// console.log(button);
+
+button.addEventListener('click', () => {
+    console.log('add clicked');
+    console.log(todoCreate.value);
+
+    let input = document.createElement('p');
+    input.innerHTML = `<input value=${todoCreate.value} disabled>`;
+
+    todoList.appendChild(input);
+
+});
+
+
+
+
+
+
