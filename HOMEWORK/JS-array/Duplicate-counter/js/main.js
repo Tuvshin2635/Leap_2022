33a2d389dc646e2b59a3bf46a92a35b875c68058
Morning <morning@MStarss-iMac.local>
2023-01-23 12:32:47 +0800

    array homweork

---
+++ HOMEWORK/JS-array/Duplicate-counter/js/main.js
@@ -0,0 +1,10 @@
+function findNumberCount(arr) {
+    for( let i = 0; i<1; i++){
+        const result = filterNumbers(arr, i);
+        const resultOb
+    }
+
+}
+
+const array = [1, 2, 3, 4, 1];
+
