9d84c1d375c4befdc03a0a1c1f6b22b732aba8b4
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2023-02-13 07:01:57 +0800

    day14js

--- JS/day-14/js/assignment.js
+++ JS/day-14/js/assignment.js
@@ -7,23 +7,23 @@
 //     console.log(a.charAt(count));
 //     sum = sum + Number(a.charAt(count));
 //     count = count + 1;
-// } 
+// }
 // console.log(sum);
 
 // for ( let a ='123'; let count=0; count<5; count++) {
 
 // }
-let n = Number(prompt('give number?'));
-let sum = 0;
-let rem = 0;
+// let n = Number(prompt('give number?'));
+// let sum = 0;
+// let rem = 0;
 
-;
-while (n>0) {
-    rem = n%10;
-    sum=sum+rem;
-    n=Math.floor(n/10);
-}
-console.log(sum);
+// ;
+// while (n>0) {
+//     rem = n%10;
+//     sum=sum+rem;
+//     n=Math.floor(n/10);
+// }
+// console.log(sum);
 
 // let sum = 0, rem = 0;
 // let N = Number(prompt('give number?'));
@@ -33,4 +33,3 @@ console.log(sum);
 //     sum = sum + rem;
 //     N = Math.floor(N / 10)
 // }   console.log(sum);
-
--- JS/day-14/js/main.js
+++ JS/day-14/js/main.js
@@ -1,80 +1,99 @@
-// // let count = 0;
-// // let sum = 0;
+let count = 0;
+let sum = 0;
 
-// // while (count < 10) {
-// //     count = count + 1;
-// //     sum = sum + count;
-// //     console.log(sum);
-// //     // console.log(count);
-// // }
-// // tegsh too gargax
-// // const max = 10;
-// // let start = 0;
-// // while (start <= 10) {
-// //     if (start % 2 == 0) {
-// //         console.log(start);
-// //     } start = start + 1;
-// // }
-// // tegsh too gargax for using
-// console.log('for version tegsh too gargax');
-// for (let start=0; start < 10; start++ ) {
-//     if (start % 2 == 0){
-//         console.log(start)
-//     }
+// while (count < 10) {
+//   count = count + 1;
+//   sum = sum + count;
+//   console.log(sum);
+// console.log(count);
+// }
+// tegsh too gargax
+// const max = 10;
+// let start = 0;
+// while (start <= 10) {
+//   if (start % 2 == 1) {
+//     console.log(start);
+//   }
+//   start = start + 1;
+// }
+// console.log(start);
+// tegsh too gargax for using
+// console.log("for version tegsh too gargax");
+// for (let start = 0; start < 10; start++) {
+//   if (start % 2 == 0) {
+//     console.log(start);
+//   }
 // }
 
-
-
-
-// // for ashiglax 
-// console.log('for version 5 xvrtel');
+// for ashiglax/
+// console.log("for version 5 xvrtel");
 // let xyz = 5;
-// for (let i = 1; i<=xyz; i++ ) {
-//     console.log(i);
+// for (let i = 1; i <= xyz; i++) {
+//   console.log(i);
 // }
 
+// sondgoi too gargax
 
+// const MIN = 20;
+// let minStart = 1;
+// while (minStart <= MIN) {
+//   if (minStart % 1 == 0) {
+//     console.log(minStart);
+//   }
+//   minStart = minStart + 1;
+// }
+// console.log("for version sondgoi too");
 
+// useg doosh tsuvuulj bicheh
 
+// const str = "life is Beautiful";
 
-//     // sondgoi too gargax
+// let urt = str.length;
+// let letCount = 0;
+// while (letCount < urt) {
+//   console.log(str.charAt(letCount));
+//   letCount = letCount + 1;
+// }
 
-// //     const MIN = 20;
-// // let minStart = 1;
-// // while (minStart <= MIN) {
-// //     if (minStart % 2 !== 0) {
-// //         console.log(minStart);
-// //     }
-// //     minStart = minStart + 1;
-// // }
-// console.log('for version sondgoi too');
+//factariol too olox
 
+// let f = Number(prompt("give me number?"));
+// let fcount = 1;
+// while (fcount < f) {
+//   if ((fcount = fcount * (fcount + 1))) {
+//     console.log(fcount);
+//   }
+//   fcount = fcount + 1;
+// }
 
+// async function fryEgg() {
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("done!"), 1000);
+//   });
+//   // setTimeout(() => reject("unDone"), 3000);
+//   let result = await promise; // энд 1 сэкүнд хүлээнэ
+//   console.log(result);
+// }
+// console.log("a");
+// console.log("b");
+// fryEgg();
+// console.log("final");
 
+function prepare_breakfast() {
+  let coffee_promise = new Promise((resolve, reject) =>
+    resolve("coffee ready")
+  );
+  let egg_promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("finished frying egg");
+    }, 1000);
+  });
+  let combined_promise = Promise.all([coffee_promise, egg_promise]);
+  return combined_promise;
+}
 
-
-// // useg doosh tsuvuulj bicheh
-
-// // const str = ('life is Beautiful');
-
-// // let urt = str.length;
-// // let letCount = 0;
-// // while (letCount < urt) {
-// //     console.log(str.charAt(letCount));
-// //     letCount = letCount + 1;
-// // }
-
-// //factariol too olox
-
-// // let f = Number(prompt('give me number?'));
-// // let fcount = 1;
-// // while (fcount < f) {
-// //     if (fcount=fcount * (fcount+1)) {
-// //         console.log(fcount);
-// //     }
-// //     fcount=fcount+1;
-// // } 
-
-
-
-
+async function morning() {
+  let result = await prepare_breakfast();
+  console.log(result);
+}
+morning();
